85c9bad9d8d0f0b8681368add4785fbc860b5057
VinoliaBattiri <vinoliabattiri@gmail.com>
2023-05-02 19:17:19 -0400

    pytest testing for flasksample

--- flask_sample/main.py
+++ flask_sample/main.py
@@ -19,7 +19,6 @@ def create_app(config_filename=''):
         app.register_blueprint(hello)
         return app
 
-
 app = create_app()
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
