97b5a7575f95d510ab87fc62dc50043f8640ad7b
VinoliaBattiri <123420587+VinoliaBattiri@users.noreply.github.com>
2023-05-06 17:51:35 -0400

    milestone2

--- Project/shop/shop.py
+++ Project/shop/shop.py
@@ -75,7 +75,7 @@ def items():
 def shop_list():
     rows = []
     args = []
-    ##UCID: ac2526 Date Dec 17th
+    ##UCID: vb437 Date May 5
     name = request.args.get("name")
     category = request.args.get("category")
     price = request.args.get("price")
@@ -185,7 +185,7 @@ def purchase():
         DB.getDB().autocommit = False # make a transaction
 
         # get cart to verify
-        ## UCID: ac2526, Date: Dec 21
+        ## UCID: vb437, Date May 5
         result = DB.selectAll("""SELECT c.id, product_id, name, c.desired_quantity, i.stock, c.unit_price as cart_cost, i.unit_price as item_cost, (c.desired_quantity * c.unit_price) as subtotal 
         FROM IS601_S_Cart c JOIN IS601_S_Products i on c.product_id = i.id
         WHERE c.user_id = %s
@@ -213,8 +213,7 @@ def purchase():
         # create order data
         order_id = -1
         if not has_error:
-            ##UCID: ac2526, Date: Dec21
-            address = request.form.get('apt') + "," + request.form.get('city') + "," + request.form.get('state') + "," + request.form.get('country') + "," + request.form.get('zpcode')
+            ##UCID: vb437, Date May 5            address = request.form.get('apt') + "," + request.form.get('city') + "," + request.form.get('state') + "," + request.form.get('country') + "," + request.form.get('zpcode')
             payment_method = request.form.get('paymentmethod')
             money_received = request.form.get('amount')
             fname = request.form.get('fname')
@@ -247,7 +246,7 @@ def purchase():
                 DB.getDB().rollback()
         # update stock based on cart data
         if not has_error:
-            ##UCID: ac2526, Date: Dec 21
+            ##UCID: vb437, Date May 5
             result = DB.update("""
             UPDATE IS601_S_Products 
                 set stock = stock - (select IFNULL(desired_quantity, 0) FROM IS601_S_Cart WHERE product_id = IS601_S_Products.id and user_id = %(uid)s) 
